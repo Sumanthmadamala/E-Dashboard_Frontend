5a1820514e72730feae3a34b9cff9f3344315a2e
VENKATA SUMANTH MADAMALA <sumanth6993@gmail.com>
2024-09-13 20:00:47 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,20 +1,20 @@
-import { React, useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-const Signup = () => {
+
+const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
   useEffect(() => {
     const auth = localStorage.getItem("user");
     if (auth) {
       navigate("/");
     }
-  });
+  }, []);
 
   const collectData = async () => {
-    console.log(name, email, password);
+    console.warn(name, email, password);
     let result = await fetch("http://localhost:5000/register", {
       method: "post",
       body: JSON.stringify({ name, email, password }),
@@ -24,38 +24,40 @@ const Signup = () => {
     });
     result = await result.json();
     console.warn(result);
-    localStorage.setItem("user", JSON.stringify(result));
+    localStorage.setItem("user", JSON.stringify(result.result));
+    localStorage.setItem("token", JSON.stringify(result.auth));
+
     navigate("/");
   };
 
   return (
     <div className="register">
-      <h1>SignUp</h1>
+      <h1>Register</h1>
       <input
         className="inputBox"
         type="text"
+        placeholder="Enter Name"
         value={name}
-        onChange={(event) => setName(event.target.value)}
-        placeholder="Enter Your Name"
+        onChange={(e) => setName(e.target.value)}
       />
       <input
         className="inputBox"
         type="text"
+        placeholder="Enter Email"
         value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        placeholder="Enter Your Email"
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         className="inputBox"
         type="password"
+        placeholder="Enter password"
         value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        placeholder="Create Your Password "
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={collectData} className="appButton">
-        SignUp
+      <button onClick={collectData} className="appButton" type="button">
+        Sign Up
       </button>
     </div>
   );
 };
-export default Signup;
+export default SignUp;
